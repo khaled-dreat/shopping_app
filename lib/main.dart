a78645833fae2fc0f8635ac7f30f4e934e6a0090
khaled-dreat <dreatkhaled@gmail.com>
2024-03-20 21:52:54 +0200

    set up Bloc observer in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:shopping_app/core/import/app_import.dart';
 
 void main() {
+  // * Stripe Initialized
   WidgetsFlutterBinding.ensureInitialized();
   Stripe.publishableKey = StripeKey.publishableKey;
-  //
-
+  // * Bloc Observer
+  Bloc.observer = SimpleBlocObserver();
+  // * Run App
   runApp(const AppStart());
 }
