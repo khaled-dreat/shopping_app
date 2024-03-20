ef8fe85d41e638235d1a0ee54cc124974118f235
khaled-dreat <dreatkhaled@gmail.com>
2024-03-21 01:23:32 +0200

    Add customerID field to PaymentIntentInputModel

--- lib/features/checkout/data/model/payment_intent_input_models.dart
+++ lib/features/checkout/data/model/payment_intent_input_models.dart
@@ -1,13 +1,19 @@
 class PaymentIntentInputModel {
   final String amount;
   final String currency;
+  final String customerID;
 
-  PaymentIntentInputModel({required this.amount, required this.currency});
+  PaymentIntentInputModel({
+    required this.amount,
+    required this.currency,
+    required this.customerID,
+  });
 
   toJson() {
     return {
       'amount': '${amount}00',
       'currency': currency,
+      'customer': customerID
     };
   }
 }
